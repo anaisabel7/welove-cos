7e3793c50706f7a398b11b4a0d151b14d3f06031
Ana Isabel <imacswv@gmail.com>
2018-03-17 12:24:16 +0100

    Added app quotes with model Quote. Included app in installed apps. Made model accesible from admin.

---
+++ welove_cos/quotes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Quote(models.Model):
+    quote_text = models.CharField(max_length=600)
+    source = models.CharField(max_length=100)
+
+    def __str__(self):
+        returned_str = "A quote from %s" % (self.source)
+        return returned_str
