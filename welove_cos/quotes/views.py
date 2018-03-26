096ccab4e45b68c609b60dc814eebcf178a800d1
Ana Isabel <imacswv@gmail.com>
2018-03-26 22:31:01 +0200

    Changed view to work with template. Added corresponding basic template

--- welove_cos/quotes/views.py
+++ welove_cos/quotes/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Quote
 
 
 def index(request):
     random_quote = Quote.objects.order_by('?')[0]
-    output = "A random quote from City Of Sound is \"%s\"" % random_quote.quote_text
-    return HttpResponse(output)
+    template = loader.get_template('quotes/index.html')
+    context = {
+        'quote_of_the_day': random_quote,
+    }
+    return HttpResponse(template.render(context, request))
