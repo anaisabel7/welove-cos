521e857ac0ff35ba0d4d855a4748408071bc49e8
Ana Isabel <imacswv@gmail.com>
2018-04-06 12:55:58 +0200

    Setting DEBUG=False in preparation to deploy in Heroku

--- welove_cos/welove_cos/settings.py
+++ welove_cos/welove_cos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e*n7&$5k)587^#)$&v#qw4xl8kyb-*0pb)v0k1)losjnuireiu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
