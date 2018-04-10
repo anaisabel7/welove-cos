b51f672dbfda12aebd3dc07fb8154df598b961c3
Ana Isabel <imacswv@gmail.com>
2018-04-10 13:50:26 +0200

    Changed database to postgresql, recomended for heroku & added local to allowed hosts

--- welove_cos/welove_cos/settings.py
+++ welove_cos/welove_cos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e*n7&$5k)587^#)$&v#qw4xl8kyb-*0pb)v0k1)losjnuireiu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'welove_cos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'welove_cosdb',
         'USER': 'cos_lover',
         'PASSWORD': 'loveoverfear',
