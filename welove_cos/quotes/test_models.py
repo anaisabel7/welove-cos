0a0cb891084c00bf59c848f9b6e3809e25fc52e1
Ana Isabel <imacswv@gmail.com>
2018-04-20 15:30:12 +0200

    Previous tests passing

--- welove_cos/quotes/test_models.py
+++ welove_cos/quotes/test_models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from mock import patch
-from .models import Quote
+from .models import Quote, Source
 from .tests import QuoteReadyTestCase
 
 
@@ -8,7 +8,7 @@ class QuoteTest(QuoteReadyTestCase):
     def test_quote_fields(self):
         fields = {
             'quote_text': models.CharField,
-            'source': models.CharField,
+            'source': models.ForeignKey,
             'selected': models.BooleanField
         }
         for name in fields:
@@ -22,8 +22,8 @@ class QuoteTest(QuoteReadyTestCase):
         selected_default = Quote._meta.get_field('selected')._get_default()
         self.assertEqual(selected_default, False)
 
-        source_max_length = Quote._meta.get_field('source').max_length
-        self.assertEqual(source_max_length, 100)
+        source_related_model = Quote._meta.get_field('source').related_model
+        self.assertEqual(source_related_model, Source)
 
         quote_text_max_length = Quote._meta.get_field('quote_text').max_length
         self.assertEqual(quote_text_max_length, 600)
@@ -31,4 +31,7 @@ class QuoteTest(QuoteReadyTestCase):
     def test_str_method(self):
         quote = self.create_quote()
         quote_str = quote.__str__()
-        self.assertEqual(quote_str, "A quote from {}".format(quote.source))
+        self.assertEqual(
+            quote_str,
+            "A quote from {}".format(quote.source.name)
+        )
--- welove_cos/quotes/tests.py
+++ welove_cos/quotes/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Quote
+from .models import Quote, Source
 
 # Create your tests here.
 
@@ -7,10 +7,20 @@ from .models import Quote
 class QuoteReadyTestCase(TestCase):
 
     quote_text = "This band rocks"
-    source = "Me"
+    source_name = "Me"
     selected = False
+    link = ''
 
-    def create_quote(self, text=quote_text, source=source, selected=selected):
+    def create_source(self, name=source_name, link=link):
+        source_object = Source.objects.create(
+            name=name,
+            link=link,
+        )
+        return source_object
+
+    def create_quote(self, text=quote_text, source=None, selected=selected):
+        if source is None:
+            source = self.create_source()
         quote_object = Quote.objects.create(
             quote_text=text,
             source=source,
