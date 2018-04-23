1e217913565ac94b213ffca58cb397dece4481db
Ana Isabel <imacswv@gmail.com>
2018-04-23 12:54:36 +0200

    Correcting error in css

--- welove_cos/welove_cos/settings.py
+++ welove_cos/welove_cos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e*n7&$5k)587^#)$&v#qw4xl8kyb-*0pb)v0k1)losjnuireiu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
 ALLOWED_HOSTS = ["*"]
