2601ceea43344c31219b23310b7f78aa9a24d130
Ana Isabel <imacswv@gmail.com>
2018-04-25 13:20:30 +0200

    Removing sending email from task

--- welove_cos/quotes/tasks.py
+++ welove_cos/quotes/tasks.py
@@ -1,15 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from django.conf.settings import EMAIL_HOST_USER as email_user
-from django.core.mail import EmailMessage
 from .models import Quote
-
-
-def email_notification_selected_quote(quote):
-    body = "Task get_random_quote has just selected this quote: {}".format(
-        quote.quote_text
-    )
-    email = EmailMessage("New Quote Selected", body, to=[email_user])
 
 
 @shared_task
@@ -21,7 +12,5 @@ def get_random_quote():
     random_quote = Quote.objects.order_by('?')[0]
     random_quote.selected = True
     random_quote.save()
-
-    email_notification_selected_quote(random_quote)
 
     print("{} - Selected".format(random_quote.quote_text))
