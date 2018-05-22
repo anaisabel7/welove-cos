689fda6ea1775bb35fa36633dff2d2ebd51f7d59
Ana Isabel <imacswv@gmail.com>
2018-05-22 11:11:42 +0200

    Added a function to email the admin with warning as well as printing it

---
+++ welove_cos/quotes/common.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+
+def warning_email_admin(warning_text="WARNING: Unknown warning"):
+    print(warning_text)
+
+    admin_email = settings.EMAIL_HOST_USER
+    title = "New Warning in {}".format(settings.SITE_DOMAIN)
+    body_intro = "A new warning was detected in {}.\n".format(
+        settings.SITE_DOMAIN
+    )
+    body_middle = "This is its content:\n{}\n".format(
+        warning_text
+    )
+    body_ending = "Warnings often do not need you to take extra actions."
+    body = "{}{}{}".format(
+        body_intro, body_middle, body_ending
+    )
+    email_to_send = EmailMessage(title, body, to=[admin_email])
+    email_to_send.send()
