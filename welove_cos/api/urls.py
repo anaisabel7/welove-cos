f1b0620b828ba86155e967712bc4d29ec6f8fc0b
Ana Isabel <imacswv@gmail.com>
2018-07-31 20:43:04 +0200

    Created apis to list all quotes and get the detail of a single quote

--- welove_cos/api/urls.py
+++ welove_cos/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from api.views import DailyApiView
+from api.views import DailyApiView, SingleQuoteApiView, AllQuotesApiView
 
 urlpatterns = [
-    path('daily', DailyApiView.as_view(), name='daily_api')
+    path('daily', DailyApiView.as_view(), name='daily_api'),
+    path(
+        'quote/<int:pk>',
+        SingleQuoteApiView.as_view(),
+        name='single_quote_api'
+    ),
+    path('all_quotes', AllQuotesApiView.as_view(), name='all_quotes_api')
 ]
--- welove_cos/api/views.py
+++ welove_cos/api/views.py
@@ -7,3 +7,15 @@ class DailyApiView(generics.ListAPIView):
 
     queryset = Quote.objects.filter(selected=True)
     serializer_class = QuoteSerializer
+
+
+class AllQuotesApiView(generics.ListAPIView):
+
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
+
+
+class SingleQuoteApiView(generics.RetrieveAPIView):
+
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
